bb3af812a8b3489744a7a5d547711814f71468b6
pjhanks <pjhanks@uwm.edu>
2022-04-20 15:31:48 -0500

    Added fields for Course and TA to try to get testing to work.

--- TAScheduler/models.py
+++ TAScheduler/models.py
@@ -2,28 +2,33 @@ from django.db import models
 
 # Create your models here.
 
+
 class MyUser(models.Model):
-  userID = models.CharField(max_length=9, primary_key=True)
-  name = models.CharField(max_length=20)
-  address = models.CharField(max_length=40)
-  email = models.CharField(max_length=20)
-  phoneNumber = models.CharField(max_length=20)
-  role = models.CharField(max_length=11)
-  password = models.CharField(max_length=20)
+    userID = models.CharField(max_length=9, primary_key=True)
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=40)
+    email = models.CharField(max_length=20)
+    phoneNumber = models.CharField(max_length=20)
+    role = models.CharField(max_length=11)
+    password = models.CharField(max_length=20)
+
 
 class TA(models.Model):
-  TACode = models.CharField(max_length=20, primary_key=True, default="42")
+    TACode = models.CharField(max_length=20, primary_key=True, default="42")
+
 
 class Course(models.Model):
-  courseCode = models.CharField(max_length=9, primary_key=True, default="12345")
-  instructorID = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
-  TAname = models.ForeignKey(TA, on_delete=models.CASCADE, null=True)
-  courseNumber = models.CharField(max_length=20, default="101")
+    courseCode = models.CharField(max_length=9, primary_key=True, default="12345")
+    instructorID = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
+    TAname = models.ForeignKey(TA, on_delete=models.CASCADE, null=True)
+    courseNumber = models.CharField(max_length=20, default="101")
+
 
 class Sections(models.Model):
-  sectionCode=models.CharField(max_length=10, primary_key=True)
-  parentCode = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
-  TA = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
+    sectionCode=models.CharField(max_length=10, primary_key=True)
+    parentCode = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    TA = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
+
 
 class classTAAssignments(models.Model):
-  pass
+    pass
