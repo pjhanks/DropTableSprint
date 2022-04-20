71eb98b3432f824f36fb086cbe1cbb4cbe3766c1
pjhanks <pjhanks@uwm.edu>
2022-04-20 09:51:00 -0500

    Login unit tests. Updating setup to work properly. Updated models to help with unit tests.

--- TAScheduler/models.py
+++ TAScheduler/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class MyUser(models.Model):
   userID = models.CharField(max_length=9, primary_key=True)
   name = models.CharField(max_length=20)
@@ -8,17 +9,21 @@ class MyUser(models.Model):
   email = models.CharField(max_length=20)
   phoneNumber = models.CharField(max_length=20)
   role = models.CharField(max_length=11)
-
-class Course(models.Model):
-  pass
+  password = models.CharField(max_length=20)
 
 class TA(models.Model):
-  pass
+  TACode = models.CharField(max_length=20, primary_key=True, default="42")
+
+class Course(models.Model):
+  courseCode = models.CharField(max_length=9, primary_key=True, default="12345")
+  instructorID = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
+  TAname = models.ForeignKey(TA, on_delete=models.CASCADE, null=True)
+  courseNumber = models.CharField(max_length=20, default="101")
 
 class Sections(models.Model):
   sectionCode=models.CharField(max_length=10, primary_key=True)
   parentCode = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
-  TA = models.ForeignKey(MyUser,on_delete=models.CASCADE, null=True)
+  TA = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
 
 class classTAAssignments(models.Model):
   pass
