7ca7f76b6ecbe893121e85f56d1621b11347bf00
pjhanks <pjhanks@uwm.edu>
2022-04-20 15:30:15 -0500

    Imported and registered models in admin.py

--- TAScheduler/admin.py
+++ TAScheduler/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import MyUser, TA, Course, Sections, classTAAssignments
 # Register your models here.
+admin.site.register(MyUser)
+admin.site.register(TA)
+admin.site.register(Course)
+admin.site.register(Sections)
+admin.site.register(classTAAssignments)
