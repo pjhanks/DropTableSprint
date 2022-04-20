ff0c99c5578b2be94a6d71b3046d211953edcb88
pjhanks <pjhanks@uwm.edu>
2022-04-20 15:32:32 -0500

    Added admin path and courses path.

--- TAScheduler/urls.py
+++ TAScheduler/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
+
 urlpatterns = [
-                path('',views.myView, name='home'),
-            ]
+    path('admin/', admin.site.urls),
+    path('',views.Home, name='home'),
+    path('courses/', views.Home.as_view())
+]
