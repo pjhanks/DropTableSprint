2c8b76725c7729ddc9968bdff4e8db6b82015f1a
pjhanks <pjhanks@uwm.edu>
2022-04-20 15:33:16 -0500

    Defined Home(View) class with get/post methods from lab (updated with our naming conventions).

--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import MyUser
 # Create your views here.
 from django.http import HttpResponse
 
+class Home(View):
+    def get(self, request):
+        return render(request, "home.html")
 
-def myView(request):
-    return render(request,"home.html",{})
+    def post(self, request):
+        noSuchUser = False
+        badPassword = False
+        try:
+            m = MyUser.objects.get(name=request.POST['name'])
+            badPassword = (m.password != request.POST['password'])
+        except:
+            noSuchUser = True
+        if noSuchUser:
+            m = MyUser(name=request.POST['name'], password=request.POST['password'])
+            m.save()
+            request.session["name"] = m.name
+            return redirect("/courses/")
+        elif badPassword:
+            return render(request, "home.html", {"message": "bad password"})
+        else:
+            request.session["name"] = m.name
+            return redirect("/courses/")
+
+    def myView(request):
+         return render(request, "home.html", {})
