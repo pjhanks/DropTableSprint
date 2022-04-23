8f4697084c812bf56a1358999b6a568580b104a6
pjhanks <pjhanks@uwm.edu>
2022-04-23 11:34:47 -0500

    Changed field name in TA to TAID and in ClassTAAssignments to TACode.

--- TAScheduler/models.py
+++ TAScheduler/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class MyUser(models.Model):
-    userID = models.CharField(max_length=9, primary_key=True)
+    IDNumber = models.CharField(max_length=9, primary_key=True)
     name = models.CharField(max_length=20)
     address = models.CharField(max_length=40)
     email = models.CharField(max_length=20)
@@ -15,20 +15,32 @@ class MyUser(models.Model):
 
 class TA(models.Model):
     TACode = models.CharField(max_length=20, primary_key=True, default="42")
+    TAID = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.TACode
 
 class Course(models.Model):
     courseCode = models.CharField(max_length=9, primary_key=True, default="12345")
     instructorID = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
-    TAname = models.ForeignKey(TA, on_delete=models.CASCADE, null=True)
+    TAs = models.ForeignKey(TA, on_delete=models.CASCADE, null=True)
     courseNumber = models.CharField(max_length=20, default="101")
 
+    def __str__(self):
+        return self.courseCode
 
 class Sections(models.Model):
-    sectionCode=models.CharField(max_length=10, primary_key=True)
+    sectionCode = models.CharField(max_length=10, primary_key=True)
     parentCode = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
-    TA = models.ForeignKey(MyUser, on_delete=models.CASCADE, null=True)
+    TA = models.ForeignKey(TA, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.sectionCode
 
-class classTAAssignments(models.Model):
-    pass
+class ClassTAAssignments(models.Model):
+    AssignmentsID = models.CharField(max_length=50, primary_key=True)
+    courseCode = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    TAcode = models.ForeignKey(TA, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.AssignmentsID
