38497168565c739d7f34002931ad5efa87a50432
pjhanks <pjhanks@uwm.edu>
2022-04-23 10:33:22 -0500

    Added createTA, createCourse, createSections, createClassTAAssignments.

--- TAScheduler/Users.py
+++ TAScheduler/Users.py
@@ -1,7 +1,42 @@
-from .models import Users
-class User():
+from .models import MyUser, TA, Course, Sections, ClassTAAssignments
+class Users():
   
-  def createUser(self, ID, name,address,email, phoneNumber, role):
-    #temp = MyUser("USER INFO")
-    #temp.save
-    pass
+  def createUser(self, IDnumber, name, address, email, phoneNumber, role, password):
+    check = list(map(str, MyUser.objects.filter(ID=IDnumber)))
+    if len(check)==0:
+      raise Exception("Database already contains someone with that ID")
+    else:
+      temp = MyUser(IDnumber, name, address, email, phoneNumber, role, password)
+      temp.save()
+
+  def createTA(self, TACode):
+    check = list(map(str, TA.objects.filter(ID=TACode)))
+    if len(check)==0:
+      raise Exception("Database already contains a TA with that ID")
+    else:
+      temp = TA(TACode)
+      temp.save()
+
+  def createCourse(self, courseCode, instructorID, TAs, courseNumber):
+    check = list(map(str, Course.objects.filter(ID=courseCode)))
+    if len(check)==0:
+      raise Exception("Database already contains a course with that ID")
+    else:
+      temp = Course(courseCode, instructorID, TAs, courseNumber)
+      temp.save()
+
+  def createSections(self, sectionCode, parentCode, TA):
+    check = list(map(str, Sections.objects.filter(ID=sectionCode)))
+    if len(check)==0:
+      raise Exception("Database already contains a section with that ID")
+    else:
+      temp = Sections(sectionCode, parentCode, TA)
+      temp.save()
+
+  def createClassTAAssignments(self, AssignmentsID, courseCode, TAID):
+    check = list(map(str, Sections.objects.filter(ID=AssignmentsID)))
+    if len(check) == 0:
+      raise Exception("Database already contains a classTAAssignment with that ID")
+    else:
+      temp = Sections(AssignmentsID, courseCode, TAID)
+      temp.save()
