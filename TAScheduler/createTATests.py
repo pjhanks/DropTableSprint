5d7cb97a79b92c27b9ca7598cbb620f2bb0e612a
pjhanks <pjhanks@uwm.edu>
2022-04-23 10:59:42 -0500

    Unit tests for createTA.

---
+++ TAScheduler/createTATests.py
@@ -0,0 +1,49 @@
+import unittest
+import Users
+from django.test import TestCase
+from .models import TA
+
+
+class testPositive(TestCase):
+    TAList = None
+
+    def setUp(self):
+        temp = TA(ID="1",
+                  TAID="7")
+        temp.save()
+
+        temp = TA(ID="2",
+                  TAID="11")
+        temp.save()
+
+
+    def test_addTA(self):
+        # add Something.addTA("TAcode");
+
+        Users.createTA("3", "8")
+        things = list(map(str, TA.objects.filter(TAID="8")))
+        self.assertIn("3", things, "TA was not added to list")
+
+    def test_intID(self):
+        # non string
+        with self.assertRaises(TypeError, msg="The values passed to createTA must be strings!"):
+            Users.createTA(3, "8")
+
+    def test_floatID(self):
+        # non string
+        with self.assertRaises(TypeError, msg="The values passed to createTA must be strings!"):
+            Users.createTA(3.0, "8")
+
+    def test_badID(self):
+        # same ids
+        Users.createTA("2", "42")
+        things = list(map(str, TA.objects.filter(name="2")))
+        self.assertIn("2", things, "Item was improperly added to list")
+
+    def test_toManyFields(self):
+        with self.assertRaises(TypeError, msg="Too many arguments passed for createTA!"):
+            Users.createTA("3", "7", "park place")
+
+    def test_toLittleFields(self):
+        with self.assertRaises(TypeError, msg="Not enough arguments passed for createTA!"):
+            Users.createUser("3")
