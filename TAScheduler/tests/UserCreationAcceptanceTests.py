f58bf302d480ff47aa37ec1e1fa02f45f9a1694d
alexMFuller <alex_fuller@ymail.com>
2022-04-25 18:05:54 -0500

    File reorganization / attempted to fix user creation tests

--- TAScheduler/tests/UserCreationAcceptanceTests.py
+++ TAScheduler/tests/UserCreationAcceptanceTests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase, Client
 from TAScheduler.models import MyUser
 
+
 class testPositive(TestCase):
     def setUp(self):
-        self.mockClient=Client()
-        temp = MyUser(ID="1",
+        self.mockClient = Client()
+        temp = MyUser(IDNumber="1",
                       name="Fred",
                       address="123 park place",
                       email="fred@uwm.edu",
@@ -12,7 +13,7 @@ class testPositive(TestCase):
                       role="Supervisor",
                       password="1234")
         temp.save()
-        temp = MyUser(ID="2",
+        temp = MyUser(IDNumber="2",
                       name="Alex",
                       address="124 park place",
                       email="alex@uwm.edu",
@@ -20,34 +21,40 @@ class testPositive(TestCase):
                       role="TA",
                       password="1234")
         temp.save()
-        self.client.post("/",{"InputUsername":"1","InputPassword":"1234"},follow=True)
+        self.client.post("/", {"InputUsername": "1", "InputPassword": "1234"}, follow=True)
 
     def test_addTA(self):
         # add Something.addUser("user info");
-        resp = self.mockClient.post("makeUser/",{"InputIDNumber":"3","InputName":"Ronen","InputAddress":"122 Park place","InputEmail":"a@uwm","InputPhoneNumber":"18000000000","InputRole":"TA","InputPassword":"1234"},follow=True)
+        resp = self.mockClient.post("/makeUser/",
+                                    {"InputIDNumber": "3", "InputName": "Ronen", "InputAddress": "122 Park place",
+                                     "InputEmail": "a@uwm", "InputPhoneNumber": "18000000000", "InputRole": "TA",
+                                     "InputPassword": "1234"}, follow=True)
         checkUser = MyUser.objects.get(IDNumber="3")
-        self.assertIn("3", checkUser, "Item was not added to list")
+        self.assertIn("3", checkUser.IDNumber, "Item was not added to list")
+
     def test_addInstructor(self):
-        resp = self.mockClient.post("makeUser/",
+        resp = self.mockClient.post("/makeUser/",
                                     {"InputIDNumber": "4", "InputName": "Ronen", "InputAddress": "122 Park place",
-                                     "InputEmail": "a@uwm", "InputPhoneNumber": "18000000000", "InputRole": "Instructor",
+                                     "InputEmail": "a@uwm", "InputPhoneNumber": "18000000000",
+                                     "InputRole": "Instructor",
                                      "InputPassword": "1234"}, follow=True)
 
         checkUser = MyUser.objects.get(IDNumber="4")
         self.assertIn("4", checkUser, "Item was not added to list")
+
     def test_addSupervisor(self):
-        resp = self.mockClient.post("makeUser/",
+        resp = self.mockClient.post("/makeUser/",
                                     {"InputIDNumber": "5", "InputName": "Ronen", "InputAddress": "122 Park place",
-                                     "InputEmail": "a@uwm", "InputPhoneNumber": "18000000000", "InputRole": "Supervisor",
+                                     "InputEmail": "a@uwm", "InputPhoneNumber": "18000000000",
+                                     "InputRole": "Supervisor",
                                      "InputPassword": "1234"}, follow=True)
 
         checkUser = MyUser.objects.get(IDNumber="5")
         self.assertIn("5", checkUser, "Item was not added to list")
 
 
-
 class testNegative(TestCase):
-    def setup(self):
+    def setUp(self):
         self.mockClient = Client()
         temp = MyUser(IDNumber="1",
                       name="Fred",
@@ -66,10 +73,9 @@ class testNegative(TestCase):
                       password="1234")
         temp.save()
 
-
     def test_notSupervisor(self):
         # same ids
-        self.client.post("/",{"InputUsername":"2","InputPassword": "1234"},follow=True)
+        self.client.post("/", {"InputUsername": "2", "InputPassword": "1234"}, follow=True)
         resp = self.mockClient.post("makeUser/",
                                     {"InputIDNumber": "3", "InputName": "Ronen", "InputAddress": "122 Park place",
                                      "InputEmail": "a@uwm", "InputPhoneNumber": "18000000000", "InputRole": "TA",
@@ -79,7 +85,7 @@ class testNegative(TestCase):
         self.assertNotIn("3", checkUser, "Item was added and should not have")
 
     def test_notFullyFilledOut(self):
-        self.client.post("/",{"InputUsername":"1","InputPassword":"1234"},follow=True)
+        self.client.post("/", {"InputUsername": "1", "InputPassword": "1234"}, follow=True)
         resp = self.mockClient.post("makeUser/",
                                     {"InputIDNumber": "4", "InputName": "", "InputAddress": "122 Park place",
                                      "InputEmail": "a@uwm", "InputPhoneNumber": "18000000000", "InputRole": "TA",
