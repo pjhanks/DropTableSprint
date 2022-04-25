e2528b9a8158ed6f4255f9c798e9a0558acecaff
Rloundy <rloundy@uwm.edu>
2022-04-25 16:17:55 -0500

    Created assign instructor

--- TAScheduler/Courses.py
+++ TAScheduler/Courses.py
@@ -3,14 +3,14 @@ from .models import Course, MyUser
 
 class CoursesClass():
 
-    def createCourse(self, CourseCode, instructorID, CourseNumber):
+    def createCourseTeacher(self, CourseCode, instructorID, CourseNumber):
         if (CourseCode is None):
             raise Exception("does not accept null Primary keys")
         check = list(map(str, Course.objects.filter(courseCode=CourseCode)))
         checkInstructor = list(map(str, MyUser.objects.filter(ID=instructorID)))
-        if len(checkInstructor == 0):
+        if len(checkInstructor) > 0:
             raise Exception("No such parent course")
-        if len(check) == 0:
+        if len(check) > 0:
             raise Exception("Database already contains am course with that ID")
         else:
             temp = Course(CourseCode, instructorID, CourseNumber)
@@ -20,8 +20,12 @@ class CoursesClass():
         if (CourseCode is None):
             raise Exception("does not accept null Primary keys")
         check = list(map(str, Course.objects.filter(courseCode=CourseCode)))
-        if len(check) == 0:
+        if len(check) > 0:
             raise Exception("Database already contains am course with that ID")
         else:
             temp = Course(CourseCode, None, CourseNumber)
             temp.save()
+    def assignInstructor(self, CourseCode, InstructorID):
+        toUpdate = Course.objects.get(courseCode=CourseCode)
+        toUpdate.instructorID=InstructorID
+        toUpdate.save()
