6e66aebb97d8815c7e37aeb130bc94d8d3530684
alexMFuller <alex_fuller@ymail.com>
2022-04-26 15:42:28 -0500

    fix course creation acceptance tests

--- TAScheduler/tests/CourseCreationAcceptanceTests.py
+++ TAScheduler/tests/CourseCreationAcceptanceTests.py
@@ -1,96 +1,106 @@
 from django.test import TestCase, Client
-from TAScheduler.models import MyUser,Course
+from TAScheduler.models import MyUser, Course
+
 
 class testPositive(TestCase):
     def setUp(self):
-        self.mockClient=Client()
-        instr = MyUser(IDNumber="1",
-                      name="Fred",
-                      address="123 park place",
-                      email="fred@uwm.edu",
-                      phoneNumber="18000000000",
-                      role="Instructor",
-                      password="1234")
-        instr.save()
-        admin = MyUser(IDNumber="2",
-                      name="Fred",
-                      address="123 park place",
-                      email="fred@uwm.edu",
-                      phoneNumber="18000000000",
-                      role="Supervisor",
-                      password="1234")
-        admin.save()
-        temp = Course(courseCode="1",
-                      instructorID="1",
-                      CourseNumber="3")
-        temp.save()
-        self.mockClientclient.post("/", {"InputUsername": "2", "InputPassword": "1234"}, follow=True)
+        self.mockClient = Client()
 
-    def addCourseWithoutInstructor(self):
-        resp = self.mockClient.post("makeCourse/",{"InputCourseCode":"3","InputCourseNumber":"101","InputInstructor":None},follow = True)
+        self.myuser = MyUser.objects.create(IDNumber="1",
+                                            name="Fred",
+                                            address="123 park place",
+                                            email="fred@uwm.edu",
+                                            phoneNumber="18000000000",
+                                            role="Instructor",
+                                            password="1234")
+        self.myuser.save()
+
+        self.admin = MyUser.objects.create(IDNumber="2",
+                                           name="Fred",
+                                           address="123 park place",
+                                           email="fred@uwm.edu",
+                                           phoneNumber="18000000000",
+                                           role="Supervisor",
+                                           password="1234")
+        self.admin.save()
+
+        self.courses = Course.objects.create(courseCode="1",
+                                             instructorID=self.myuser,
+                                             courseNumber="3")
+
+        self.courses.save()
+
+        self.mockClient.post("/", {"InputUsername": "2", "InputPassword": "1234"}, follow=True)
+
+    def test_addCourseWithoutInstructor(self):
+        resp = self.mockClient.post("/makeCourse/",
+                                    {"InputCourseCode": "3", "InputCourseNumber": "101", "InputInstructor": ""},
+                                    follow=True)
         checkCourse = Course.objects.get(courseCode="3")
-        self.assertIn("3", checkCourse, "Item was not added to list")
-    def addCourseWithInstructor(self):
-        resp = self.mockClient.post("makeCourse/",
+        self.assertEqual("3", checkCourse.courseCode, "Item was not added to list")
+
+    def test_addCourseWithInstructor(self):
+        resp = self.mockClient.post("/makeCourse/",
                                     {"InputCourseCode": "4", "InputCourseNumber": "101", "InputInstructor": "1"},
                                     follow=True)
+
         checkCourse = Course.objects.get(courseCode="4")
-        self.assertIn("4", checkCourse, "Item was not added to list")
-    def removeCourse(self):
-        resp = self.mockClient.post("removeCourse/", {"InputCourseCode": "1"})
-        checkCourse = Course.objects.get(courseCode="1")
-        self.assertNotIn("1", checkCourse, "Course was not removed")
+        self.assertEqual("4", checkCourse.courseCode, "Item was not added to list")
+
+    def test_removeCourse(self):
+        resp = self.mockClient.post("/removeCourse/", {"InputCourse": "1"})
+        checkCourse = Course.objects.filter(courseCode="1")
+        self.assertEqual(0, checkCourse.count(), "Course was not removed")
+
 
 class testNegative(TestCase):
-        def setup(self):
-            self.mockClient = Client()
-            instr = MyUser(IDNumber="1",
-                           name="Fred",
-                           address="123 park place",
-                           email="fred@uwm.edu",
-                           phoneNumber="18000000000",
-                           role="Instructor",
-                           password="1234")
-            instr.save()
-            admin = MyUser(IDNumber="2",
-                           name="Fred",
-                           address="123 park place",
-                           email="fred@uwm.edu",
-                           phoneNumber="18000000000",
-                           role="Supervisor",
-                           password="1234")
-            admin.save()
-            TA = MyUser(IDNumber="3",
-                           name="Fred",
-                           address="123 park place",
-                           email="fred@uwm.edu",
-                           phoneNumber="18000000000",
-                           role="TA",
-                           password="1234")
-            temp = Course(courseCode="1",
-                          instructorID="1",
-                          courseNumber="3")
-            temp.save()
-        def testLoggedInAsTA(self):
-            self.mockClient.post("/", {"InputUsername": "3", "InputPassword": "1234"}, follow=True)
-            resp = self.mockClient.post("makeCourse/",
-                                        {"InputCourseCode": "5", "InputCourseNumber": "101", "InputInstructor": None},
-                                        follow=True)
-            checkCourse = Course.objects.get(courseCode="5")
-            self.assertNotIn("5", checkCourse, "Item was not supposed added to list")
-        def NotFullyFilledOut(self):
-            self.mockClient.post("/", {"InputUsername": "2", "InputPassword": "1234"}, follow=True)
-            resp = self.mockClient.post("makeCourse/",
-                                        {"InputCourseCode": "6", "InputCourseNumber": "", "InputInstructor": None},
-                                        follow=True)
-            checkCourse = Course.objects.get(courseCode="6")
-            self.assertNotIn("7", checkCourse, "Item was not supposed added to list")
-            resp = self.mockClient.post("makeCourse/",
-                                        {"InputCourseCode": "7", "InputCourseNumber": None, "InputInstructor": None},
-                                        follow=True)
-            checkCourse = Course.objects.get(courseCode="7")
-            self.assertNotIn("6", checkCourse, "Item was not supposed added to list")
-        def noSuchCourse(self):
-            resp = self.mockClient.post("removeCourse/", {"InputCourseCode": "3"})
-            checkCourse = Course.objects.get(courseCode="3")
-            self.assertNotIn("3", checkCourse, "Course was added somehow")
+    def setUp(self):
+        self.mockClient = Client()
+
+        self.myuser = MyUser.objects.create(IDNumber="1",
+                                            name="Fred",
+                                            address="123 park place",
+                                            email="fred@uwm.edu",
+                                            phoneNumber="18000000000",
+                                            role="Instructor",
+                                            password="1234")
+        self.myuser.save()
+
+        self.admin = MyUser.objects.create(IDNumber="2",
+                                           name="Fred",
+                                           address="123 park place",
+                                           email="fred@uwm.edu",
+                                           phoneNumber="18000000000",
+                                           role="Supervisor",
+                                           password="1234")
+        self.admin.save()
+
+        self.courses = Course.objects.create(courseCode="1",
+                                             instructorID=self.myuser,
+                                             courseNumber="3")
+
+        self.courses.save()
+
+    def test_testLoggedInAsTA(self):
+        self.mockClient.post("/", {"InputUsername": "3", "InputPassword": "1234"}, follow=True)
+        x = self.mockClient.session["username"] = "3"
+        resp = self.mockClient.get("/makeCourse/", {"username": x})
+
+        checkCourse = Course.objects.get(courseCode="5")
+
+        self.assertEqual("5", checkCourse.courseCode, "Item was not supposed added to list")
+
+    def test_NotFullyFilledOut(self):
+        self.mockClient.post("/", {"InputUsername": "2", "InputPassword": "1234"}, follow=True)
+        resp = self.mockClient.post("/makeCourse/",
+                                    {"InputCourseCode": "6", "InputCourseNumber": "", "InputInstructor": ""},
+                                    follow=True)
+        checkCourse = Course.objects.get(courseCode="6")
+        self.assertNotEqual("6", checkCourse.courseCode, "Item was not supposed added to list")
+        resp = self.mockClient.post("/makeCourse/",
+                                    {"InputCourseCode": "7", "InputCourseNumber": "", "InputInstructor": ""},
+                                    follow=True)
+        checkCourse = Course.objects.get(courseCode="7")
+        self.assertNotEqual("7", checkCourse.courseCode, "Item was not supposed added to list")
+
+
