ce7b12312b3d55c3d0f6a1ada664bd8f84e7fbc2
alexMFuller <alex_fuller@ymail.com>
2022-05-05 16:00:10 -0500

    Changes to users page -added user skills column

--- TAScheduler/admin.py
+++ TAScheduler/admin.py
@@ -3,9 +3,13 @@ from .models import MyUser
 from .models import Sections
 from .models import ClassTAAssignments
 from .models import Course
+from .models import UserSkills
+from .models import Skills
 
 # Register your models here.
 admin.site.register(MyUser)
 admin.site.register(Sections)
 admin.site.register(ClassTAAssignments)
 admin.site.register(Course)
+admin.site.register(UserSkills)
+admin.site.register(Skills)
