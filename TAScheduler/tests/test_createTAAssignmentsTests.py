c8ae364620a786a000ec995fd576c92cc1281ef8
alexMFuller <alex_fuller@ymail.com>
2022-05-15 14:51:05 -0500

    fixes for createTAAssingmentsTests

--- TAScheduler/tests/test_createTAAssignmentsTests.py
+++ TAScheduler/tests/test_createTAAssignmentsTests.py
@@ -1,49 +1,103 @@
 import unittest
-import Users
+from TAScheduler.classes import Users
 from django.test import TestCase
-from TAScheduler.models import ClassTAAssignments
+from TAScheduler.models import ClassTAAssignments, Course, MyUser
 
 
 class testClassTAAssignments(TestCase):
     ClassTAList = None
 
     def setUp(self):
-        temp = ClassTAAssignments(ID="1",
-                      courseCode="7",
-                      TAcode="39")
+        temp1 = MyUser(IDNumber="1",
+                       name="Fred",
+                       address="123 park place",
+                       email="fred@uwm.edu",
+                       phoneNumber="18000000000",
+                       role="Supervisor",
+                       password="123")
+        temp1.save()
+        temp2 = MyUser(IDNumber="2",
+                       name="Alex",
+                       address="124 park place",
+                       email="alex@uwm.edu",
+                       phoneNumber="18000000001",
+                       role="TA",
+                       password="123")
+        temp2.save()
+        temp = MyUser(IDNumber="3",
+                      name="Bob",
+                      address="879 place",
+                      email="bob@uwm.edu",
+                      phoneNumber="182354231",
+                      role="Instructor",
+                      password="123")
         temp.save()
 
-        temp = ClassTAAssignments(ID="2",
-                      courseCode="8",
-                      TAcode="32")
+        course8 = Course(courseCode="8",
+                      instructorID=temp1,
+                      courseNumber="101")
+        temp.save()
+        course7 = Course(courseCode="7",
+                      instructorID=temp2,
+                      courseNumber="315")
+        course7.save()
+
+        ta39 = MyUser(IDNumber="39",
+                      name="Fred",
+                      address="123 park place",
+                      email="fred@uwm.edu",
+                      phoneNumber="18000000000",
+                      role="TA",
+                      password="1234")
+        ta39.save()
+
+        ta32 = MyUser(IDNumber="32",
+                      name="Fred",
+                      address="123 park place",
+                      email="fred@uwm.edu",
+                      phoneNumber="18000000000",
+                      role="TA",
+                      password="1234")
+        ta32.save()
+
+
+
+        temp = ClassTAAssignments(AssignmentsID="1",
+                      courseCode=course7,
+                      TAcode=ta39)
+        temp.save()
+
+        temp = ClassTAAssignments(AssignmentsID="2",
+                      courseCode=course8,
+                      TAcode=ta32)
         temp.save()
 
     def test_addClassTA(self):
         # add Something.addClassTA("AssignmentsID");
-        Users.createClassTAAssignments("3", "9", "44")
+        Users.UserClass.createClassTAAssignments("3", "9", "44")
         things = list(map(str, ClassTAAssignments.objects.filter(courseCode="9")))
         self.assertIn("3", things, "Class TA Assignment was not added to list")
 
     def test_intID(self):
         # non string
         with self.assertRaises(TypeError, msg="The values passed to createClassTAAssignments must be strings!"):
-            Users.createClassTAAssignments(3, "9", "44")
+            Users.UserClass.createClassTAAssignments(3, "9", "44")
 
     def test_floatID(self):
         # non string
         with self.assertRaises(TypeError, msg="The values passed to createClassTAAssignments must be strings!"):
-            Users.createClassTAAssignments(3.0, "9", "44")
+            Users.UserClass.createClassTAAssignments(3.0, "9", "44")
 
     def test_badID(self):
         # same ids
-        Users.createClassTAAssignments("2", "9", "42")
+        Users.UserClass.createClassTAAssignments("2", "9", "42")
         things = list(map(str, ClassTAAssignments.objects.filter(name="2")))
         self.assertIn("2", things, "Class TA Assignment was improperly added to list")
 
     def test_toManyFields(self):
         with self.assertRaises(TypeError, msg="Too many arguments passed for createClassTAAssignments!"):
-            Users.createClassTAAssignments("3", "9", "44", "oops extra argument")
+            Users.UserClass.createClassTAAssignments("3", "9", "44", "oops extra argument")
 
     def test_toLittleFields(self):
         with self.assertRaises(TypeError, msg="Not enough arguments passed for createClassTAAssignments!"):
-            Users.createClassTAAssignments("3")
+            Users.UserClass.createClassTAAssignments("3")
