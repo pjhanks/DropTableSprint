58015b343e6b2a15a407596914ef3dde89d6c610
wardre <wardre@uwm.edu>
2022-05-16 22:46:35 -0500

    changed up change and remove instructor acceptance tests

--- TAScheduler/tests/RemoveInstructorsAcceptanceTests.py
+++ TAScheduler/tests/RemoveInstructorsAcceptanceTests.py
@@ -5,34 +5,34 @@ from TAScheduler.models import MyUser, Course
 class testPositive(TestCase):
     def setUp(self):
         self.mockClient = Client()
-        temp1 = MyUser(IDNumber="1",
+        self.instr1 = MyUser.objects.create(IDNumber="1",
                        name="Fred",
                        address="123 park place",
                        email="fred@uwm.edu",
                        phoneNumber="18000000000",
                        role="Instructor",
                        password="1234")
-        temp1.save()
-        temp2 = MyUser(IDNumber="2",
+        self.instr1.save()
+        self.instr2 = MyUser.objects.create(IDNumber="2",
                        name="John",
                        address="123 park place",
                        email="john@uwm.edu",
                        phoneNumber="18000000000",
                        role="Instructor",
                        password="1234")
-        temp2.save()
-        temp3 = Course(courseCode="351",
+        self.instr2.save()
+        self.courses1 = Course.objects.create(courseCode="351",
                        instructorID=None,
                        courseNumber="101")
-        temp3.save()
-        temp4 = Course(courseCode="479",
-                       instructorID=temp2,
+        self.courses1.save()
+        self.courses2 = Course.objects.create(courseCode="479",
+                       instructorID=self.instr2,
                        courseNumber="101")
-        temp4.save()
-        temp5 = Course(courseCode="337",
-                       instructorID=temp1,
+        self.course2.save()
+        self.instr3 = Course.objects.create(courseCode="337",
+                       instructorID=self.instr1,
                        courseNumber="101")
-        temp5.save()
+        self.instr3.save()
         self.mockClient.post("/", {"InputCourseCode": "1"}, follow=True)
 
     def test_removeInstructor(self):
@@ -44,39 +44,38 @@ class testPositive(TestCase):
 class testNegative(TestCase):
     def setUp(self):
         self.mockClient = Client()
-        temp1 = MyUser(IDNumber="1",
-                       name="Fred",
-                       address="123 park place",
-                       email="fred@uwm.edu",
-                       phoneNumber="18000000000",
-                       role="Instructor",
-                       password="1234")
-        temp1.save()
-        temp2 = MyUser(IDNumber="2",
-                       name="Fred",
-                       address="123 park place",
-                       email="fred@uwm.edu",
-                       phoneNumber="18000000000",
-                       role="Supervisor",
-                       password="1234")
-        temp2.save()
-
-        temp3 = Course(courseCode="351",
-                       instructorID=None,
-                       courseNumber="3")
-        temp3.save()
-        temp4 = Course(courseCode="479",
-                       instructorID=temp2,
-                       courseNumber="101")
-        temp4.save()
-        temp5 = Course(courseCode="337",
-                       instructorID=temp1,
-                       courseNumber="101")
-        temp5.save()
+        self.instr1 = MyUser.objects.create(IDNumber="1",
+                                            name="Fred",
+                                            address="123 park place",
+                                            email="fred@uwm.edu",
+                                            phoneNumber="18000000000",
+                                            role="Instructor",
+                                            password="1234")
+        self.instr1.save()
+        self.instr2 = MyUser.objects.create(IDNumber="2",
+                                            name="John",
+                                            address="123 park place",
+                                            email="john@uwm.edu",
+                                            phoneNumber="18000000000",
+                                            role="Instructor",
+                                            password="1234")
+        self.instr2.save()
+        self.courses1 = Course.objects.create(courseCode="351",
+                                              instructorID=None,
+                                              courseNumber="101")
+        self.courses1.save()
+        self.course2 = Course.objects.create(courseCode="479",
+                                              instructorID=self.instr2,
+                                              courseNumber="101")
+        self.course2.save()
+        self.instr3 = Course.objects.create(courseCode="337",
+                                            instructorID=self.instr1,
+                                            courseNumber="101")
+        self.instr3.save()
         self.mockClient.post("/", {"InputCourseCode": "1"}, follow=True)
 
     def test_unassignNotFullyFilledOut(self):
-        self.mockClientclient.post("/", {"InputCourseCode": "2"}, follow=True)
+        self.mockClient.post("/", {"InputCourseCode": "2"}, follow=True)
         resp = self.mockClient.post("/removeInstructor/", {"InputSectionCode": ""}, follow=True)
         checkCourse = Course.objects.get(courseCode="")
         self.assertNotIn("", checkCourse, "Item was not added to list")
@@ -85,13 +84,13 @@ class testNegative(TestCase):
         self.assertNotIn(None, checkCourse, "Item was not added to list")
 
     def test_NoInstructorToRemove(self):
-        self.mockClientclient.post("/", {"InputCourseCode": "3"}, follow=True)
+        self.mockClient.post("/", {"InputCourseCode": "3"}, follow=True)
         resp = self.mockClient.post("/removeInstructor/", {"InputCourseCode": "3"}, follow=True)
         checkCourse = Course.objects.get(courseCode="3")
         self.assertNotIn("3", checkCourse, "Item was not added to list")
 
     def test_noCourseEntered(self):
-        self.mockClientclient.post("/", {"InputCourseCode": "2"}, follow=True)
+        self.mockClient.post("/", {"InputCourseCode": "2"}, follow=True)
         resp = self.mockClient.post("/removeInstructor/", follow=True)
         checkCourse = Course.objects.get(courseCode="")
         self.assertNotIn("", checkCourse, "Item was missing Course")
--- TAScheduler/tests/changeInstructorsAcceptanceTests.py
+++ TAScheduler/tests/changeInstructorsAcceptanceTests.py
@@ -5,34 +5,34 @@ from TAScheduler.models import MyUser, Course
 class testPositive(TestCase):
     def setUp(self):
         self.mockClient = Client()
-        temp1 = MyUser(IDNumber="1",
-                       name="Fred",
-                       address="123 park place",
-                       email="fred@uwm.edu",
-                       phoneNumber="18000000000",
-                       role="Instructor",
-                       password="1234")
-        temp1.save()
-        temp2 = MyUser(IDNumber="2",
-                       name="John",
-                       address="123 park place",
-                       email="john@uwm.edu",
-                       phoneNumber="18000000000",
-                       role="Instructor",
-                       password="1234")
-        temp2.save()
-        temp3 = Course(courseCode="351",
-                       instructorID=None,
-                       courseNumber="101")
-        temp3.save()
-        temp4 = Course(courseCode="479",
-                       instructorID=temp2,
-                       courseNumber="101")
-        temp4.save()
-        temp5 = Course(courseCode="337",
-                       instructorID=temp1,
-                       courseNumber="101")
-        temp5.save()
+        self.instr1 = MyUser.objects.create(IDNumber="1",
+                                            name="Fred",
+                                            address="123 park place",
+                                            email="fred@uwm.edu",
+                                            phoneNumber="18000000000",
+                                            role="Instructor",
+                                            password="1234")
+        self.instr1.save()
+        self.instr2 = MyUser.objects.create(IDNumber="2",
+                                            name="John",
+                                            address="123 park place",
+                                            email="john@uwm.edu",
+                                            phoneNumber="18000000000",
+                                            role="Instructor",
+                                            password="1234")
+        self.instr2.save()
+        self.courses1 = Course.objects.create(courseCode="351",
+                                              instructorID=None,
+                                              courseNumber="101")
+        self.courses1.save()
+        self.courses2 = Course.objects.create(courseCode="479",
+                                              instructorID=self.instr2,
+                                              courseNumber="101")
+        self.courses2.save()
+        self.courses3 = Course.objects.create(courseCode="337",
+                                            instructorID=self.instr1,
+                                            courseNumber="101")
+        self.courses3.save()
         self.mockClient.post("/", {"InputCourseCode": "1", "InputIDNumber": "2"}, follow=True)
 
     def test_changeInstructor(self):
@@ -43,35 +43,34 @@ class testPositive(TestCase):
 class testNegative(TestCase):
     def setUp(self):
         self.mockClient = Client()
-        temp1 = MyUser(IDNumber="1",
-                       name="Fred",
-                       address="123 park place",
-                       email="fred@uwm.edu",
-                       phoneNumber="18000000000",
-                       role="Instructor",
-                       password="1234")
-        temp1.save()
-        temp2 = MyUser(IDNumber="2",
-                       name="Fred",
-                       address="123 park place",
-                       email="fred@uwm.edu",
-                       phoneNumber="18000000000",
-                       role="Supervisor",
-                       password="1234")
-        temp2.save()
-
-        temp3 = Course(courseCode="351",
-                       instructorID=None,
-                       courseNumber="3")
-        temp3.save()
-        temp4 = Course(courseCode="479",
-                       instructorID=temp2,
-                       courseNumber="101")
-        temp4.save()
-        temp5 = Course(courseCode="337",
-                       instructorID=temp1,
-                       courseNumber="101")
-        temp5.save()
+        self.instr1 = MyUser.objects.create(IDNumber="1",
+                                            name="Fred",
+                                            address="123 park place",
+                                            email="fred@uwm.edu",
+                                            phoneNumber="18000000000",
+                                            role="Instructor",
+                                            password="1234")
+        self.instr1.save()
+        self.instr2 = MyUser.objects.create(IDNumber="2",
+                                            name="John",
+                                            address="123 park place",
+                                            email="john@uwm.edu",
+                                            phoneNumber="18000000000",
+                                            role="Instructor",
+                                            password="1234")
+        self.instr2.save()
+        self.courses1 = Course.objects.create(courseCode="351",
+                                              instructorID=None,
+                                              courseNumber="101")
+        self.courses1.save()
+        self.courses2 = Course.objects.create(courseCode="479",
+                                              instructorID=self.instr2,
+                                              courseNumber="101")
+        self.courses2.save()
+        self.courses3 = Course.objects.create(courseCode="337",
+                                            instructorID=self.instr1,
+                                            courseNumber="101")
+        self.courses3.save()
         self.mockClient.post("/", {"InputCourseCode": "1", "InputIDNumber": "2"}, follow=True)
 
     def test_assignNotFullyFilledOut(self):
@@ -84,19 +83,19 @@ class testNegative(TestCase):
         self.assertNotIn("2", checkCourse, "Item was not added to list")
 
     def test_alreadyContainsSpecifiedInstructor(self):
-        self.mockClientclient.post("/", {"InputCourseCode": "2", "InputIDNumber": "1"}, follow=True)
+        self.mockClient.post("/", {"InputCourseCode": "2", "InputIDNumber": "1"}, follow=True)
         resp = self.mockClient.post("/addInstructor/", {"InputCourseCode": "1", "InputIDNumber": "1"}, follow=True)
         checkCourse = Course.objects.get(courseCode="1")
         self.assertNotIn("1", checkCourse, "Item was not added to list")
 
     def test_noInstructorAttached(self):
-        self.mockClientclient.post("/", {"InputCourseCode": "2", "InputIDNumber": "1"}, follow=True)
+        self.mockClient.post("/", {"InputCourseCode": "2", "InputIDNumber": "1"}, follow=True)
         resp = self.mockClient.post("/addInstructor/", {"InputCourseCode": "1"})
         checkCourse = Course.objects.get(courseCode="1")
         self.assertNotIn("300", checkCourse, "Item was missing Instructor ID")
 
     def test_noCourseEntered(self):
-        self.mockClientclient.post("/", {"InputCourseCode": "2", "InputIDNumber": "1"}, follow=True)
+        self.mockClient.post("/", {"InputCourseCode": "2", "InputIDNumber": "1"}, follow=True)
         resp = self.mockClient.post("/addInstructor/", {"InputCourseCode": ""}, follow=True)
         checkCourse = Course.objects.get(courseCode="")
         self.assertNotIn("", checkCourse, "Item missing Course")
